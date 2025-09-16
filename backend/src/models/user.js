93459072f969d3e4ef9de897ec56525f8d81c758
Vignesh Pai <vigneshpai27@gmail.com>
2025-09-16 18:33:39 +0530

    Fixed User Model

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -54,5 +54,5 @@ userSchema.pre("save", async function (next) {
     return await bcrypt.compare(enteredPassword, this.password);
   };
   
-const User = mongoose.model("user", userSchema);
+const User = mongoose.model("User", userSchema);
 export default User;
